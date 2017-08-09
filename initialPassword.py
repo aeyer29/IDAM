ac43eab1eb8bbf1ea94f7d6c393246a5c362faa0
Eyerman <alexandra.t.eyerman@accenture.com>
2017-08-09 17:00:02 -0400

    adding initialPassword.py

---
+++ initialPassword.py
@@ -0,0 +1,42 @@
+#Gather initialPasswordView details for list of users. 
+#Necessary inputs: 
+#    list of users, delineated by newline (\n)
+#    username of OpenIDM user to call API with
+#    password of OpenIDM user
+
+import json, subprocess, getopt, sys
+
+def main(argv): 
+	inputFile = ''
+	openidmUsername = ''
+	openidmPassword = ''
+	try: 
+		opts, args = getopt.getopt(argv, "Hi:u:p:",["inputFile=", "username=", "password="])
+	except getopt.GetoptError: 
+		print 'initialPassword.py -i <inputFile> -u <username> -p <password>'
+		sys.exit(2)
+	for opt, arg in opts: 
+		if opt == '-H':
+			print 'initialPassword.py is used to gather intial password information for a list of users.\nUsage: python initialPassword.py -i <inputFile> -u <username> -p <password>'
+			sys.exit()
+		elif opt in ('-i', '--inputFile'): 
+			inputFile = arg
+		elif opt in ('-u', '--username'):
+			openidmUsername = arg
+		elif opt in ('-p', '--password'):
+			openidmPassword = arg
+	print "inputFile is: " + str(inputFile)
+	print "username is: " + str(openidmUsername)
+	print "password is: " + str(openidmPassword)
+
+	with open(str(inputFile), 'r') as openFile:
+		readFile = openFile.read()
+
+	entries = readFile.strip().split('\n')
+
+	for entry in entries:
+		print "entry: " + str(entry)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
+
