c2df60c2e15d04c52b43bf3572449d847f2ae967
Eyerman <alexandra.t.eyerman@accenture.com>
2017-08-10 08:39:03 -0400

    added curl command

--- initialPassword.py
+++ initialPassword.py
@@ -17,7 +17,7 @@ def main(argv):
 		sys.exit(2)
 	for opt, arg in opts: 
 		if opt == '-H':
-			print 'initialPassword.py is used to gather intial password information for a list of users.' \
+			print '\ninitialPassword.py is used to gather intial password information for a list of users.' \
 			'\nUsage: python initialPassword.py -i <inputFile> -u <username> -p <password>' \
 			'\n-i	--inputFile		.txt file with list of users to process. Delineated by linebreaks.' \
 			'\n-u 	--username 		The login username for OpenIDM to use for the API call.' \
@@ -29,9 +29,9 @@ def main(argv):
 			openidmUsername = arg
 		elif opt in ('-p', '--password'):
 			openidmPassword = arg
-	print "inputFile is: " + str(inputFile)
-	print "username is: " + str(openidmUsername)
-	print "password is: " + str(openidmPassword)
+	#print "inputFile is: " + str(inputFile)
+	#print "username is: " + str(openidmUsername)
+	#print "password is: " + str(openidmPassword)
 
 	with open(str(inputFile), 'r') as openFile:
 		readFile = openFile.read()
@@ -39,7 +39,14 @@ def main(argv):
 	entries = readFile.strip().split('\n')
 
 	for entry in entries:
-		print "entry: " + str(entry)
+		#print "entry: " + str(entry)
+		curlCommand='curl -H \"X-OpenIDM-Username:' + str(openidmUsername) + '\" '\
+		'-H \"X-OpenIDM-Password:' + str(openidmPassword) + '\" '\
+		'--request GET '
+		'https://sso.qa.valvoline.com/openidm/managed/user?_queryFilter=userName+eq+%22' + str(entry) + '%22&_fields=userName,initialPasswordView'
+
+		print curlCommand
+		#subprocess.call()
 
 
 
