f4f946c975d180a04ac0cf1f2f4464e0bf01b763
Eyerman <alexandra.t.eyerman@accenture.com>
2018-08-20 14:42:14 -0700

    non-shell runnable

--- collectinfo.py
+++ collectinfo.py
@@ -2,14 +2,10 @@
 
 #Update details for list of users. 
 #Necessary inputs: 
-#    list of users, delineated by newline (\n)
+#    input file
 #    output file
-#    username of OpenIDM user to call API with
-#    password of OpenIDM user
 
 import json
-import getopt
-import getpass
 import sys
 import requests
 from urllib import quote as quoteURL
@@ -23,35 +19,5 @@ def parseInput(inFile):
 	return byLines
 
 
-def main(argv): 
-	inputFile = ''
-	outputFile = ''
-
-	try: 
-		#Define arguments for command
-		opts, args = getopt.getopt(argv, "Hi:o:",["inputFile=", "outputFile="])
-	except getopt.GetoptError: 
-		print 'ucollectinfo.py -i <inputFile> -o <outputFile>'
-		sys.exit(2)
-	for opt, arg in opts: 
-		#Read args from the command
-		if opt == '-H': 
-			print '\ncollectinfo.py is used to aggregate responses in csv format' \
-			'\nUsage: ./updateUsers.py -i <inputFile> ' \
-			'\n-i   --inputFile             response file' \
-			'\n-o   --outputFile            Name of file to output. '
-	
-
-			#exit if -H is called, as this is just to print a help command
-			sys.exit()
-		elif opt in ('-i', '--inputFile'): 
-			inputFile = arg
-		elif opt in ('-o', '--outputFile'):
-			outputFile=arg
-
-	#use list(set()) to ensure each entry is distinct 
+def collectinfos(inputFile, outputFile): 
 	print(parseInput(inputFile))
-
-
-if __name__ == "__main__":
-	main(sys.argv[1:])
